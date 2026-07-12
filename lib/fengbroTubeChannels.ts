befc14e07e473e993e4df1ccb9d0664a9e505e47
goldshoot0720 <goldshoot0720@gmail.com>
2026-07-12 13:05:07 +0800

    feat(tube): remove Leonard channel

--- lib/fengbroTubeChannels.ts
+++ lib/fengbroTubeChannels.ts
@@ -20,13 +20,11 @@ export const FENGBRO_TUBE_TITLE_OVERRIDES: Record<string, string> = {
   monsterise: "怪獸崛起 MONSTERISE",
   neixianzhang: "張内咸脫口秀",
   修仙者小烨: "修仙者小烨",
-  xiaoye1757: "修炼者小烨",
   cheapaoe: "cheap",
   storytellerhk: "StorytellerHK 說書客",
   mrshenofficial: "公子沈 Terence Shen",
   gc趙氏讀書生活: "Gavinchiu趙氏讀書生活",
   tankman2020: "二爷故事",
-  leonard2834: "Leonard",
 };
 
 const DEFAULT_FENGBRO_TUBE_CHANNEL_URLS = [
@@ -45,13 +43,11 @@ const DEFAULT_FENGBRO_TUBE_CHANNEL_URLS = [
   "https://www.youtube.com/@monsterise/videos",
   "https://www.youtube.com/@NeixianZhang/videos",
   "https://www.youtube.com/@%E4%BF%AE%E4%BB%99%E8%80%85%E5%B0%8F%E7%83%A8/videos",
-  "https://www.youtube.com/@xiaoye1757/videos",
   "https://www.youtube.com/@cheapaoe/videos",
   "https://www.youtube.com/@StorytellerHK/videos",
   "https://www.youtube.com/@mrshenofficial/videos",
   "https://www.youtube.com/@GC%E8%B6%99%E6%B0%8F%E8%AE%80%E6%9B%B8%E7%94%9F%E6%B4%BB",
   "https://www.youtube.com/@Tankman2020/videos",
-  "https://www.youtube.com/@leonard2834/videos",
 
 ];
 
