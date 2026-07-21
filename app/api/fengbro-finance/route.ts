f858c82c4ebf9d019d5607619e85f15a7135cc09
goldshoot0720 <goldshoot0720@gmail.com>
2026-07-21 23:32:55 +0800

    chore(finance): update KOSPI Bilibili vt param
    Replace bilibiliUrl vt token for 韓國股市 search.

--- app/api/fengbro-finance/route.ts
+++ app/api/fengbro-finance/route.ts
@@ -144,7 +144,7 @@ const INSTRUMENTS: FinanceInstrument[] = [
     ],
     youtubeUrl: "https://www.youtube.com/results?search_query=%EC%BD%94%EC%8A%A4%ED%94%BC&sp=EgIIAg%253D%253D",
     bilibiliUrl:
-      "https://search.bilibili.com/all?vt=47771571&keyword=%E9%9F%93%E5%9C%8B%E8%82%A1%E5%B8%82&from_source=web_search&spm_id_from=333.1007&search_source=5&pubtime_begin_s=1784044800&pubtime_end_s=1784649599",
+      "https://search.bilibili.com/all?vt=47938637&keyword=%E9%9F%93%E5%9C%8B%E8%82%A1%E5%B8%82&from_source=web_search&spm_id_from=333.1007&search_source=5&pubtime_begin_s=1784044800&pubtime_end_s=1784649599",
     imageUrl: "/finance/kospi-202607201244-pink.png",
     imageUrls: [
       "/finance/kospi-202607201244-pink.png",
